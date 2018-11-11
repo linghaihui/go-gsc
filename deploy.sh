31f4a90f6fdf38c39baf8a01ba22442ed6c8f7ab
凌海辉 <linghaihui@licaigc.com>
2018-11-11 18:44:03 +0800

    update ci

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i hosts ansible.yaml -e "wxAppId=$wxAppId wxappSecret=$wxappSecret listenAddr=$listenAddr mysqlDSN=$mysqlDSN tag=release-$TRAVIS_JOB_ID"
+ansible-playbook -i hosts ansible.yaml -e "wxAppId=$wxAppId wxappSecret=$wxappSecret listenAddr=$listenAddr mysqlDSN=$mysqlDSN tag=release-$TRAVIS_JOB_ID docker_user=$USER docker_passwd=$PASSWD"
