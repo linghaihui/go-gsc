23c25d769929eee51a64c5b20e9fa6bd10958b00
凌海辉 <linghaihui@licaigc.com>
2018-11-11 18:49:52 +0800

    update ci

--- main_test.go
+++ main_test.go
@@ -3,9 +3,6 @@ package main
 import (
 	"testing"
 	"github.com/linghaihui/gogsc/util"
-	"github.com/linghaihui/gogsc/models"
-	"fmt"
-	"github.com/linghaihui/gogsc/controller"
 )
 
 func Test_DB(t *testing.T)  {
@@ -14,20 +11,4 @@ func Test_DB(t *testing.T)  {
 	}else{
 		t.Log("数据库连接正常")
 	}
-}
-
-func Test_GetGSCById(t *testing.T)  {
-	controller.GetGSCById(1, "123")
-}
-
-func Test_GetGSC30(t *testing.T)  {
-	fmt.Println(models.GetGSC30())
-}
-
-func Test_GSCQuery(t *testing.T)  {
-	fmt.Println(models.GSCQuery("宴山亭"))
-}
-
-func Test_GSCQueryLike(t *testing.T)  {
-	fmt.Println(models.GSCQueryLike("宴山亭", "123"))
 }
