426035ce81cea6fc0108bf6dcd8956e52ad2518f
凌海辉 <linghaihui@licaigc.com>
2018-11-11 18:20:51 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i hosts ansible.yaml -e "wxAppId=$wxAppId wxappSecret=$wxappSecret listenAddr=$listenAddr mysqlDSN=$mysqlDSN"
+ansible-playbook -i hosts ansible.yaml -e "wxAppId=$wxAppId wxappSecret=$wxappSecret listenAddr=$listenAddr mysqlDSN=$mysqlDSN tag=$TRAVIS_TAG"
